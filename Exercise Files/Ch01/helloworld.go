c7b08a30816a3661c6424e430799bc8477b51bc6
Carolyn Van Spyk <cvanspyk@gmail.com>
2017-03-07 21:11:34 -0800

    first three lessons

--- Exercise Files/Ch01/helloworld.go
+++ Exercise Files/Ch01/helloworld.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println(strings.ToUpper("Hello, World!"))
+
+	str1 := "The quick brown fox"
+	str2 := "jumped over"
+	str3 := "the lazy dog."
+	aNumber := 42
+	isTrue := true
+
+	stringlength, err := fmt.Println(str1, str2, str3)
+
+	if err == nil {
+		fmt.Println("String length:", stringlength)
+	}
+
+	fmt.Printf("Value of aNumber: %v\n", aNumber)
+	fmt.Printf("Value of aNumber: %v\n", isTrue)
+	fmt.Printf("Value of aNumber: %.2f\n", float64(aNumber))
+
+	// print type
+	fmt.Printf("Data types: %T, %T, %T, %T and %T\n", str1, str2, str3, aNumber, isTrue)
+	myString := fmt.Sprintf("Data types as var: %T, %T, %T, %T and %T\n", str1, str2, str3, aNumber, isTrue)
+	fmt.Print(myString)
 }
--- Exercise Files/Ch03/03_02/strings.go
+++ Exercise Files/Ch03/03_02/strings.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
-//	"strings"
+	"strings"
 )
 
 func main() {
-	
+	str1 := "An implicitly typed string"
+	fmt.Printf("str1: %v:%T\n", str1, str1)
+	fmt.Println(str1)
+
+	str2 := "An explicitly typed string"
+	fmt.Printf("str2: %v:%T\n", str2, str2)
+
+	// to uppercase
+	fmt.Println(strings.ToUpper(str1))
+	fmt.Println(strings.Title(str1))
+
+	// compare strings
+	lvalue := "hello"
+	uvalue := "HELLO"
+	fmt.Println("Equal?", (lvalue == uvalue))
+	fmt.Println("Equal non-case-sensitive?", strings.EqualFold(lvalue, uvalue))
+
+	// does one string contain another?
+	fmt.Println("Contains exp?", strings.Contains(str1, "exp"))
+	fmt.Println("Contains exp?", strings.Contains(str2, "exp"))
 }
